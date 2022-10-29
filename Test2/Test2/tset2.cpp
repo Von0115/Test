ec0ec3d23551ed1f714460188d4c9dbb639f25e2
Von0115 <372245928@qq.com>
2022-10-29 09:25:45 +0800

    测试啊啊啊啊啊啊

---
+++ Test2/Test2/tset2.cpp
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{  //定义数组
+	int arr[8] = {0};
+	//输入
+	int i = 0;
+	for(i=0;i<8;i++)
+	{
+		scanf("%d", &arr[i]);
+	}
+   //比较大小
+	int max = arr[0];
+	for (i = 0; i < 8; i++)
+	{
+		if (arr[i] > max)
+		{
+			max = arr[i];
+		}
+	}
+   //输出
+	printf("max=%d\n", max);
+
+	return 0;
+}
