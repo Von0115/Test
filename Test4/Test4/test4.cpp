8f1309c6c425e168bfe58e9837362e12acde22a0
Von0115 <372245928@qq.com>
2022-10-30 14:24:26 +0800

    判断一个数是否为奇数

---
+++ Test4/Test4/test4.cpp
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	int a = 0;
+	scanf("%d", &a);
+		if (a%2==1)
+		{
+		    printf("ÆæÊý\n");
+		}
+		else
+		{
+			printf("Å¼Êý\n");
+		}
+
+	return 0;
+}
